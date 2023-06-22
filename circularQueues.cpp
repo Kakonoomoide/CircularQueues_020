6f84039713da0c8b341f249763bc8860160262c0
Kakonoomoide <rizkiramada128@gmail.com>
2023-06-22 10:34:33 +0700

    addd func untuk insert

--- circularQueues.cpp
+++ circularQueues.cpp
@@ -6,6 +6,14 @@ private:
     int front, rear, max = 5;
     int queue_array[5];
     
+public:
+    void insert() {
+        int num;
+        cout << "enter your num: ";
+        cin >> num;
+        cout << endl;
+
+    }
 };
 
 int main(){
